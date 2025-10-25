665840a05e02517971c0f0e64bc181f4d1c9a322
Jerell James <jerell1998@gmail.com>
2025-10-25 09:04:56 +0100

    colors page

---
+++ frontend/src/app/colors/page.tsx
@@ -0,0 +1,62 @@
+export default function ColorsPage() {
+  return (
+    <div className="flex flex-col h-screen bg-white [&>div]:h-1/14 [&>div]:flex [&>div]:flex-row [&>div]:items-center [&>div]:justify-between [&>div]:font-bold [&>div]:text-white [&>div]:p-4">
+      <div className="bg-brand-gradient-1">
+        <p>1</p>
+        <p>blue</p>
+      </div>
+      <div className="bg-brand-gradient-2">
+        <p>2</p>
+        <p>indigo</p>
+      </div>
+      <div className="bg-brand-gradient-3">
+        <p>3</p>
+        <p>purple</p>
+      </div>
+      <div className="bg-brand-gradient-4">
+        <p>4</p>
+        <p>purple bright</p>
+      </div>
+      <div className="bg-brand-gradient-5">
+        <p>5</p>
+        <p>violet</p>
+      </div>
+      <div className="bg-brand-gradient-6">
+        <p>6</p>
+        <p>violet soft</p>
+      </div>
+      <div className="bg-brand-gradient-7">
+        <p>7</p>
+        <p>rose</p>
+      </div>
+      <div className="bg-brand-gradient-8">
+        <p>8</p>
+        <p>rose deep</p>
+      </div>
+      <div className="bg-brand-gradient-9">
+        <p>9</p>
+        <p>maroon</p>
+      </div>
+      <div className="bg-brand-gradient-10">
+        <p>10</p>
+        <p>brown</p>
+      </div>
+      <div className="bg-brand-gradient-11">
+        <p>11</p>
+        <p>brown deep</p>
+      </div>
+      <div className="bg-brand-gradient-12">
+        <p>12</p>
+        <p>black</p>
+      </div>
+      <div className="bg-brand-gradient-13">
+        <p>13</p>
+        <p>teal deep</p>
+      </div>
+      <div className="bg-brand-gradient-14">
+        <p>14</p>
+        <p>teal</p>
+      </div>
+    </div>
+  );
+}
