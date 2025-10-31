4dcd2ca5d7719328d704b29f7b0faebaf3f1e6e4
Jerell James <jerell1998@gmail.com>
2025-10-31 22:31:27 +0000

    removed consts page cause it doesn't work yet

--- frontend/src/app/consts/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DimConstantsForm } from "@/components/dim/constants-form";
-
-export default function ConstsPage() {
-  return (
-    <div className="flex flex-col">
-      <DimConstantsForm initial={[{ name: "c", expr: "299792458 m/s" }]} />
-    </div>
-  );
-}
